959643c4962688e17af92d7c6935a3648954ed53
Ron Park <datureezy1@Rons-MacBook-Pro.local>
2015-05-12 15:55:58 -0700

    Add profile pages for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  root to: "home#index"
-
   devise_for :users, path_names: {sign_in: "login", sign_out: "logout"}
+  resources :users, only: [:show]
+
+  root to: "home#index"
 end
